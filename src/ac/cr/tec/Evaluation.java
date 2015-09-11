4bf0eddd343f5d150805c3ad0902575455760099
damarce135 <damarce135@gmail.com>
2015-09-10 23:28:42 -0600

    Changes to Evaluation, Mainscreen

--- src/ac/cr/tec/Evaluation.java
+++ src/ac/cr/tec/Evaluation.java
@@ -47,19 +47,38 @@ public class Evaluation {
     }
 
     public String[] replace(String[] val, String a, String b){
-        if (i(a) == 0){
-            val[1] = "1";
+        if (a != null){
+            if (i(a) == 0) {
+                val[1] = "1";
+            }
+            else if (i(a) == 1){
+                val[1] = a;
+            }
+            else {
+                for (int i=2;i<=i(a);i++){
+                    val[1] = Integer.toString(i(val[1])*i(val[1]));
+                }
+            }
         }
-        if (i(b) == 0){
-            val[2] = "1";
+
+        if (b != null){
+            if (i(b) == 0) {
+                val[2] = "1";
+            }
+            else if (i(b) == 1){
+                val[2] = b;
+            }
+            else {
+                for (int i = 2;i<=i(a);i++){
+                    val[2] = Integer.toString(i(val[2])*i(val[2]));
+                }
+            }
         }
-        for (int i=1;i<i(a);i++){
-            val[1] = Integer.toString(i(val[1])*i(val[1]));
-        }
+
         return val;
     }
 
-    public void resultEval(String[] val1, String[] val2){
+    public String resultEval(String[] val1, String[] val2){
         String res= "";
         String[] entry1 = splitt(val1, 0);
         String[] entry2 = splitt(val1, 1);
@@ -67,5 +86,17 @@ public class Evaluation {
         String entry4 = val2[0];
         String entry5 = val2[1];
 
+        //After replace
+        entry1 = replace(entry1,entry4,entry5);
+        entry2 = replace(entry2,entry4,entry5);
+        entry3 = replace(entry3,entry4,entry4);
+
+        //Suma
+        int f1 = i(entry1[0])*i(entry1[1])*i(entry1[2]);
+        int f2 = i(entry2[0])*i(entry2[1])*i(entry2[2]);
+        int f3 = i(entry3[0])*i(entry3[1])*i(entry3[2]);
+        int f = f1 + f2 + f3;
+        res = Integer.toString(f);
+        return res;
     }
 }
--- src/ac/cr/tec/MainScreen.java
+++ src/ac/cr/tec/MainScreen.java
@@ -218,6 +218,11 @@ public class MainScreen {
             }
         });
 
+        evaluatesButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
     }
 
 }
